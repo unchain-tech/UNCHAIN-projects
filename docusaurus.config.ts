eda097313629da6d34c9a01abb271fa780dfcfe1
neila <40727091+neila@users.noreply.github.com>
2024-06-28 02:51:39 +0900

    fix footer link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,7 +78,7 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/unchain-tech/UNCHAIN-projects",
             },
           ],
         },
